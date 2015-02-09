fd932ff3748a6f78b9de714b584eec9b8c48388e
Chris Hasson <hassoncs@gmail.com>
2015-02-09 14:39:38 -0800

    Wrapped the kerning table parsing in a try catch to allow the rest of the font parsing to continue without kerning support.

--- src/tables/kern.js
+++ src/tables/kern.js
@@ -12,23 +12,27 @@ function parseKernTable(data, start) {
     var pairs, p, tableVersion, subTableVersion, nPairs,
         i, leftIndex, rightIndex, value;
     pairs = {};
-    p = new parse.Parser(data, start);
-    tableVersion = p.parseUShort();
-    check.argument(tableVersion === 0, 'Unsupported kern table version.');
-    // Skip nTables.
-    p.skip('uShort', 1);
-    subTableVersion = p.parseUShort();
-    check.argument(subTableVersion === 0, 'Unsupported kern sub-table version.');
-    // Skip subTableLength, subTableCoverage
-    p.skip('uShort', 2);
-    nPairs = p.parseUShort();
-    // Skip searchRange, entrySelector, rangeShift.
-    p.skip('uShort', 3);
-    for (i = 0; i < nPairs; i += 1) {
-        leftIndex = p.parseUShort();
-        rightIndex = p.parseUShort();
-        value = p.parseShort();
-        pairs[leftIndex + ',' + rightIndex] = value;
+    try {
+        p = new parse.Parser(data, start);
+        tableVersion = p.parseUShort();
+        check.argument(tableVersion === 0, 'Unsupported kern table version.');
+        // Skip nTables.
+        p.skip('uShort', 1);
+        subTableVersion = p.parseUShort();
+        check.argument(subTableVersion === 0, 'Unsupported kern sub-table version.');
+        // Skip subTableLength, subTableCoverage
+        p.skip('uShort', 2);
+        nPairs = p.parseUShort();
+        // Skip searchRange, entrySelector, rangeShift.
+        p.skip('uShort', 3);
+        for (i = 0; i < nPairs; i += 1) {
+            leftIndex = p.parseUShort();
+            rightIndex = p.parseUShort();
+            value = p.parseShort();
+            pairs[leftIndex + ',' + rightIndex] = value;
+        }
+    } catch (e) {
+        console.log('An error occured while parsing the kerning tables. No kerning will be included.', e);
     }
     return pairs;
 }
